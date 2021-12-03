ac315e14258a98f37b7d6789e4f3d10cbfa94f76
SeanIvers <seanivers55@gmail.com>
2021-12-03 16:38:47 -0500

    Start over

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -13,15 +13,58 @@ class DList:
 
     # add node to front of the list
     def add_to_front(self, val):
-        pass
+        new_node = DLNode(val)
+        if self.head == None:
+            self.head = new_node
+            return self
+        elif self.tail == None:
+            new_node.next = self.head
+            self.head.previous = new_node
+            self.head = new_node
+            self.tail = self.head.next
+            return self
+        new_node.next = self.head
+        self.head.previous = new_node
+        self.head = new_node
+        return self
 
     # add a node to the end of the list
     def add_to_back(self, val):
-        pass
+        new_node = DLNode(val)
+        if self.head == None:
+            self.head = new_node
+            return self
+        elif self.tail == None:
+            self.tail = new_node
+            self.tail.previous = self.head
+            self.head.next = self.tail
+            return self
+        new_node.previous = self.tail
+        self.tail.next = new_node
+        self.tail = new_node
+        return self
 
     # print the values
     def print_values(self):
-        pass
+        current_node = self.head
+        if self.head != None:
+            while current_node != None:
+                print(current_node.value)
+                current_node = current_node.next
+            return self
+        return self
+
+    # print the values in reverse
+    def print_reverse(self):
+        current_node = self.tail
+        if self.head != None:
+            if self.tail == None:
+                print(self.head.value)
+                return self
+            while current_node != None:
+                print(current_node.value)
+                current_node = current_node.previous
+            return self
 
     # delete an existing node
     def delete_value(self, val):
@@ -39,6 +82,9 @@ class DList:
     def is_circular(self):
         pass
 
+    def make_circular(self):
+        pass
+
     # remove duplicate values
     def remove_duplicate_values(self):
         pass
@@ -46,3 +92,8 @@ class DList:
     # reverse values in the list
     def reverse_values(self):
         pass
+
+dll = DList()
+# dll.add_to_front(3).add_to_front(2).add_to_front(1).add_to_front(0)
+# print("\n")
+dll.add_to_back(4).add_to_back(5).add_to_back(6).print_values().print_reverse()
