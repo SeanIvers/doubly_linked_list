8638b18b3ccfae7d364fb28dbba69d1383d6f696
SeanIvers <seanivers55@gmail.com>
2021-12-03 09:51:53 -0500

    add initial functions

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -11,4 +11,38 @@ class DList:
         self.head = None
         self.tail = None
 
-    
+    # add node to front of the list
+    def add_to_front(self, val):
+        pass
+
+    # add a node to the end of the list
+    def add_to_back(self, val):
+        pass
+
+    # print the values
+    def print_values(self):
+        pass
+
+    # delete an existing node
+    def delete_value(self, val):
+        pass
+
+    # insert a node between existing node with given value
+    def insert_at_value(self, val):
+        pass
+
+    # insert a node at a certain index n
+    def insert_at(self, val, n):
+        pass
+
+    # test if circular liked list
+    def is_circular(self):
+        pass
+
+    # remove duplicate values
+    def remove_duplicate_values(self):
+        pass
+
+    # reverse values in the list
+    def reverse_values(self):
+        pass
